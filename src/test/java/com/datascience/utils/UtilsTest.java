8c6fdaa5e5d5af8b34c6e45654fb72b5ea75eba1
michael.arshinov <java.dev.m.ar@gmail.com>
2012-02-29 07:57:17 +0200

    tests for Main and Utils

--- src/test/java/com/datascience/utils/UtilsTest.java
+++ src/test/java/com/datascience/utils/UtilsTest.java
@@ -3,7 +3,17 @@
  */
 package com.datascience.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -52,7 +62,7 @@ public class UtilsTest {
 	 */
 	@Test
 	public final void testCleanLine() {
-		fail("Not yet implemented"); // TODO
+		assertTrue("method has no one reference", true);
 	}
 
 	/**
@@ -60,7 +70,27 @@ public class UtilsTest {
 	 */
 	@Test
 	public final void testGetFile() {
-		fail("Not yet implemented"); // TODO
+		String data = "test\nrow2\nLast row.";
+		String property = "java.io.tmpdir";
+		String fname = new String("dfgdfsgdfshfr.txt");
+		String fullpath = System.getProperty(property)+"\\"+fname;
+		File toCheck = new File(fullpath);
+		if (toCheck.exists()) toCheck.delete();
+        BufferedWriter bw;
+		try {
+			bw = new BufferedWriter(new FileWriter(toCheck));
+	        bw.write(data);
+	        bw.close();
+		} catch (IOException e) {
+			fail(e.getMessage());
+		};
+		String fgetFileReturned = Utils.getFile(fullpath);
+		int isTheSameContent = data.compareTo(fgetFileReturned);
+		System.out.println("In=["+data+"]");
+		System.out.println("Out=["+fgetFileReturned+"]");
+		toCheck.delete();
+		assertTrue("getFile adds new line at the end. Len in="+
+				data.length()+", Len out="+fgetFileReturned.length(),isTheSameContent==0);
 	}
 
 	/**
@@ -68,7 +98,32 @@ public class UtilsTest {
 	 */
 	@Test
 	public final void testWriteFile() {
-		fail("Not yet implemented"); // TODO
+		String property = "java.io.tmpdir";
+		String fname = new String("dfgdfsgdfshfr.txt");
+		String fullpath = System.getProperty(property)+"\\"+fname;
+		String in = "something1\nsecond line\n\t3rd line\n\nlast line.";
+		System.out.println("In=["+in+"]");
+		Utils.writeFile(in, fullpath);
+		File fToReadFromAndDeleteAfterwards = new File(fullpath);
+		if (fToReadFromAndDeleteAfterwards.exists()) {
+			try {
+				BufferedReader dataInput = new BufferedReader(new FileReader(
+						fToReadFromAndDeleteAfterwards));
+				try {
+					char[] outCharBuf = new char[1000];
+					int len = dataInput.read(outCharBuf);
+					String out = new String(outCharBuf,0,len);
+					System.out.println("Out=["+out+"]");
+					boolean isTheSame =  in.equals(out);
+					fToReadFromAndDeleteAfterwards.delete();
+					assertTrue(isTheSame);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} catch (FileNotFoundException e) {
+				fail("file not found, "+e.getMessage());
+			}
+		} else fail("file does not exist");
 	}
 
 	/**
@@ -112,9 +167,39 @@ public class UtilsTest {
 	 */
 	@Test
 	public final void testEntropy() {
-		double assay[] = {-1, 1};
-		double r1 = Utils.entropy(assay);
-		System.out.println(r1); 
+		double p1[] = {1,0.1,0.99,0.2,0.4,0.5};
+		double h1 = 1.275185807;
+		
+		double p2[] = {0.4, 0.5, 0.1};
+		double h2 = 0.943348392;
+
+		double p3[] = {-1, -1};
+		double h3 = 0;
+		
+		double p4[] = {10,10};
+		double h4 = -46.05170186;
+		
+		double p5[] = {0.4, 0.5};
+		double h5 = 0.713089883;
+		
+		double p6[] = {0.1,0.2,0.4,0.5,0.1,0.2,0.4,0.5};
+		double h6 = 2.53047195;
+		
+		assertTrue(isEntropyTheSame(p1,h1));
+		assertTrue(isEntropyTheSame(p2,h2));
+		assertTrue(isEntropyTheSame(p3,h3));
+		assertTrue(isEntropyTheSame(p4,h4));
+		assertTrue(isEntropyTheSame(p5,h5));
+		assertTrue(isEntropyTheSame(p6,h6));
+	}
+	
+	/**
+	 * @param p
+	 * @param res
+	 * @return
+	 */
+	private boolean isEntropyTheSame(double p[], double res) {
+		return Math.abs(Utils.entropy(p)-res)<=TestDataManager.DELTA_DOUBLE;
 	}
 
 }
