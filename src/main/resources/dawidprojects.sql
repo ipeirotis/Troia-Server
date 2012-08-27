0a50e7e931a981ad598a0527032bf4a18e765f69
Piotr Gnys <piotr.gnys@10clouds.com>
2012-08-27 09:16:03 +0200

    Fixed id column size

--- src/main/resources/dawidprojects.sql
+++ src/main/resources/dawidprojects.sql
@@ -1,2 +1,2 @@
-create table projects ( id varchar(1000) NOT NULL PRIMARY KEY, data TEXT NOT NULL);
+create table projects ( id varchar(100) NOT NULL PRIMARY KEY, data TEXT NOT NULL);
 create index idIndex on projects (id);
