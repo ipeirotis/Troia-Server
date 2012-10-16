ba25e59f44ca1738b0279c6402eb1f30b386006d
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-10-16 15:34:43 +0200

    Better deploy script - local settings support

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,24 @@
 
 PROJECT=./
 TOMCAT=~/apache-tomcat-6.0.33/
+SETTINGS=~/.troia_settings
+
+JAVA_RESOURCES=$PROJECT/src/main/resources
+
+if [ -d "$SETTINGS" ]; then
+    BACKUP=`mktemp -d`
+    cp -rf $JAVA_RESOURCES/* $BACKUP/
+    cp -rf $SETTINGS/* $JAVA_RESOURCES/
+fi
 
 cd $PROJECT
 mvn clean
 mvn package -DskipTests=true
+
+if [ -d "$SETTINGS" ]; then
+    cp -rf $BACKUP/* $JAVA_RESOURCES/
+    rm -rf $BACKUP
+fi
 
 $TOMCAT/bin/shutdown.sh
 rm -rf $TOMCAT/webapps/GetAnotherLabel*
