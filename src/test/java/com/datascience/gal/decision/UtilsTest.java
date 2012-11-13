8bcb5a43c283316ffea36ce5727485e17083861c
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-11-13 17:06:37 +0100

    Moved test to jUnit 4 - fixed ignoring test

--- src/test/java/com/datascience/gal/decision/UtilsTest.java
+++ src/test/java/com/datascience/gal/decision/UtilsTest.java
@@ -1,13 +1,16 @@
 package com.datascience.gal.decision;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-import junit.framework.TestCase;
-
 import org.junit.Ignore;
+import org.junit.Test;
 
 import com.datascience.gal.AbstractDawidSkene;
 import com.datascience.utils.CostMatrix;
@@ -23,22 +26,13 @@ import com.datascience.utils.CostMatrix;
  *
  * @version $Revision$
  */
-public class UtilsTest extends TestCase {
-
-	/**
-	 * Construct new test instance
-	 *
-	 * @param name
-	 *            the test name
-	 */
-	public UtilsTest(String name) {
-		super(name);
-	}
+public class UtilsTest{
 
 	/**
 	 * Run the Double calculateLabelCost(String, Map<String,Double>,
 	 * CostMatrix<String>) method test
 	 */
+	@Test
 	public void testCalculateLabelCost() {
 		String calcLabel = "OK";
 		Map<String, Double> labelProbabilities = new HashMap<String, Double>();
@@ -53,13 +47,14 @@ public class UtilsTest extends TestCase {
 
 		Double result = Utils.calculateLabelCost(calcLabel, labelProbabilities,
 						costMatrix);
-		assertEquals(20.5, result);
+		assertEquals(20.5, result, 0.0);
 	}
 
 	/**
 	 * Run the Map<T,Double> generateConstantDistribution(Collection<T>, double)
 	 * method test
 	 */
+	@Test
 	public void testGenerateConstantDistribution() {
 		Collection<String> objects = new LinkedList<String>();
 		for (int i = 0; i < 10; i++) {
@@ -69,7 +64,7 @@ public class UtilsTest extends TestCase {
 		Map<String, Double> result = Utils.generateConstantDistribution(
 										 objects, value);
 		for (String s : objects) {
-			assertEquals(Math.PI, result.get(s));
+			assertEquals(Math.PI, result.get(s), 0.0);
 		}
 	}
 
@@ -77,6 +72,7 @@ public class UtilsTest extends TestCase {
 	 * Run the Map<T,Double> generateUniformDistribution(Collection<T>) method
 	 * test
 	 */
+	@Test
 	public void testGenerateUniformDistribution() {
 		Collection<String> objects = new LinkedList<String>();
 		for (int i = 0; i < 8; i++) {
@@ -84,7 +80,7 @@ public class UtilsTest extends TestCase {
 		}
 		Map<String, Double> result = Utils.generateUniformDistribution(objects);
 		for (String s : objects) {
-			assertEquals(1. / 8, result.get(s));
+			assertEquals(1. / 8, result.get(s), 0.0);
 		}
 	}
 
@@ -93,6 +89,7 @@ public class UtilsTest extends TestCase {
 	 * method test
 	 */
 	@Ignore("This might require more work ...")
+	@Test
 	public void testGetCategoriesCostMatrix() {
 		fail("Madnes ... I would have to create whole AbstractDawidSkene");
 		// add test code here
