7ee691aa3e9ae339ae84782e51a6832fb1ea85bb
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-12-27 17:35:10 +0100

    Removing dummy header

--- troia-server/src/main/java/com/datascience/gal/service/GSONSerializer.java
+++ troia-server/src/main/java/com/datascience/gal/service/GSONSerializer.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.datascience.gal.service;
 
 import com.datascience.core.storages.JSONUtils;
