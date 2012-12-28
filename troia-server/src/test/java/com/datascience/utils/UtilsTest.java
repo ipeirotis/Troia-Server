c7d9963a1d0a72e973fac3111975a794b14b3fc6
Konrad Kurdej <kkonrad@i_wont_tell.com>
2012-12-28 23:13:00 +0100

    Removing dummy code

--- troia-server/src/test/java/com/datascience/utils/UtilsTest.java
+++ troia-server/src/test/java/com/datascience/utils/UtilsTest.java
@@ -15,10 +15,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.datascience.utils.auxl.TestDataManager;
@@ -28,42 +24,6 @@ import com.datascience.utils.auxl.TestDataManager;
  *
  */
 public class UtilsTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link com.datascience.utils.Utils#cleanLine(java.lang.String)}.
-	 */
-	@Test
-	public final void testCleanLine() {
-		assertTrue("method has no one reference", true);
-	}
 
 	/**
 	 * Test method for {@link com.datascience.utils.Utils#getFile(java.lang.String)}.
