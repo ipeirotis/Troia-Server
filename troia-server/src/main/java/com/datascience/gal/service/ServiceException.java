dd943398e5f8bd2fed4f96442fca8effb35bac5d
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-12-28 11:30:00 +0100

    Removing dummy header

--- troia-server/src/main/java/com/datascience/gal/service/ServiceException.java
+++ troia-server/src/main/java/com/datascience/gal/service/ServiceException.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.datascience.gal.service;
 
 import javax.ws.rs.WebApplicationException;
