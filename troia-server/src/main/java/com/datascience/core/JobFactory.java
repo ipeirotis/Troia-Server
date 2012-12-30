64628cb564ed1e3518d723a827a14bb27f60a462
Konrad Kurdej <kkonrad@i_wont_tell.com>
2012-12-30 14:34:15 +0100

    JobFactory can create jobs with categories

--- troia-server/src/main/java/com/datascience/core/JobFactory.java
+++ troia-server/src/main/java/com/datascience/core/JobFactory.java
@@ -2,7 +2,9 @@ package com.datascience.core;
 
 import com.datascience.gal.AbstractDawidSkene;
 import com.datascience.gal.BatchDawidSkene;
+import com.datascience.gal.Category;
 import com.datascience.gal.IncrementalDawidSkene;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,8 +13,9 @@ import java.util.Map;
  */
 public class JobFactory {
 
-	private interface Creator{
+	protected interface Creator{
 		AbstractDawidSkene create(String id);
+		AbstractDawidSkene create(String id, Collection<Category> categories);
 	}
 	
 	final static Map<String, Creator> DS_FACTORY = new HashMap();
@@ -23,6 +26,11 @@ public class JobFactory {
 			public AbstractDawidSkene create(String id) {
 				return new BatchDawidSkene(id);
 			}
+
+			@Override
+			public AbstractDawidSkene create(String id, Collection<Category> categories) {
+				return new BatchDawidSkene(id, categories);
+			}
 		});
 		
 		DS_FACTORY.put("incremental", new Creator() {
@@ -31,15 +39,32 @@ public class JobFactory {
 			public AbstractDawidSkene create(String id) {
 				return new IncrementalDawidSkene(id);
 			}
+
+			@Override
+			public AbstractDawidSkene create(String id, Collection<Category> categories) {
+				return new IncrementalDawidSkene(id, categories);
+			}
 		});
 	};
 	
-	public Job createJob(String type, String id){
+	protected Creator getCreator(String type){
 		type = type.toLowerCase();
-		if (!DS_FACTORY.containsKey(type)) {
-			return null;
+		Creator creator = DS_FACTORY.get(type);
+		if (creator == null) {
+			throw new IllegalArgumentException("Uknown Job type: " + type);
 		}
-		AbstractDawidSkene ads = DS_FACTORY.get(type).create(id);
+		return creator;
+	}
+
+	public Job createJob(String type, String id){
+		Creator creator = getCreator(type);
+		AbstractDawidSkene ads = creator.create(id);
+		return new Job(ads, id);
+	}
+
+	public Job createJob(String type, String id, Collection<Category> categories){
+		Creator creator = getCreator(type);
+		AbstractDawidSkene ads = creator.create(id, categories);
 		return new Job(ads, id);
 	}
 }
