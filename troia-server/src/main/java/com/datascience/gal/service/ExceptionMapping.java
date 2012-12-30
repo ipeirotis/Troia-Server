94fc0ced97968336c76f782c2979002df8a02d26
Konrad Kurdej <kkonrad@i_wont_tell.com>
2012-12-30 14:57:27 +0100

    Another error mapping - NoSuchElementException -> 404

--- troia-server/src/main/java/com/datascience/gal/service/ExceptionMapping.java
+++ troia-server/src/main/java/com/datascience/gal/service/ExceptionMapping.java
@@ -1,5 +1,6 @@
 package com.datascience.gal.service;
 
+import java.util.NoSuchElementException;
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
@@ -28,6 +29,8 @@ public class ExceptionMapping implements ExceptionMapper<Exception>{
 		Integer status_code = null;
 		if (e instanceof IllegalArgumentException) {
 			status_code = Response.Status.BAD_REQUEST.getStatusCode();
+		} else if (e instanceof NoSuchElementException) {
+			status_code = Response.Status.NOT_FOUND.getStatusCode();
 		}
 		if (status_code != null) {
 			return responser.makeErrorResponse(status_code, e.getMessage());
