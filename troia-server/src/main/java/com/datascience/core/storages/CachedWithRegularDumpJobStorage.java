ad9ea91fcec1659cb5b3b6c610db3df278c56b53
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-01-10 18:15:48 +0100

    TROIA-178 continued

--- troia-server/src/main/java/com/datascience/core/storages/CachedWithRegularDumpJobStorage.java
+++ troia-server/src/main/java/com/datascience/core/storages/CachedWithRegularDumpJobStorage.java
@@ -1,25 +1,54 @@
 package com.datascience.core.storages;
 
+import com.google.common.util.concurrent.AbstractScheduledService;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author konrad
  */
 public class CachedWithRegularDumpJobStorage extends CachedJobStorage{
 	
-	public CachedWithRegularDumpJobStorage(final IJobStorage cachedJobStorage, int cacheSize){
+	protected AbstractScheduledService dumpingService;
+	
+	public CachedWithRegularDumpJobStorage(final IJobStorage cachedJobStorage, int cacheSize,
+			long period, TimeUnit unit){
 		super(cachedJobStorage, cacheSize);
-		
+		dumpingService = new DumpingService(period, unit);
+		start();
+	}
+	
+	private void start(){
+		// TODO
 	}
 	
 	@Override
 	public void stop() throws Exception {
-		int i = 0;
-		// xxxx
+		dumpingService.stopAndWait();
 		super.stop();
 	}
 	
 	@Override
 	public String toString() {
 		return "CachedWithRegularDump" + cachedJobStorage.toString();
+	}
+	
+	public class DumpingService extends AbstractScheduledService {
+		
+		protected Scheduler scheduler;
+		
+		public DumpingService(long delay, TimeUnit unit) {
+			scheduler = AbstractScheduledService.Scheduler.newFixedDelaySchedule(delay, delay, unit);
+		}
+
+		@Override
+		protected void runOneIteration() throws Exception {
+			// TODO
+		}
+
+		@Override
+		protected Scheduler scheduler() {
+			return scheduler;
+		}
 	}
 }
