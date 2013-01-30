a999442b6ca0b870eb1037226d9293bcde036449
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-01-30 13:59:46 +0100

    Extending Item with evaluation and gold capabilites

--- troia-server/src/main/java/com/datascience/core/base/Item.java
+++ troia-server/src/main/java/com/datascience/core/base/Item.java
@@ -3,11 +3,14 @@ package com.datascience.core.base;
 import com.google.common.base.Objects;
 
 /**
+ * In equal, hashcode we ignore evaluation and gold label
  * @Author: konrad
  */
-public class Item {
+public class Item<T> {
 
 	protected String name;
+	protected Label<T> goldLabel;
+	protected Label<T> evaluationLabel;
 
 	public Item(String name){
 		this.name = name;
@@ -19,6 +22,30 @@ public class Item {
 
 	public String getName(){
 		return name;
+	}
+
+	public boolean isGold(){
+		return goldLabel == null;
+	}
+
+	public boolean isEvaluation(){
+		return evaluationLabel == null;
+	}
+
+	public Label<T> getGoldLabel(){
+		return goldLabel;
+	}
+
+	public void setGoldLabel(Label<T> label){
+		goldLabel = label;
+	}
+
+	public Label<T> getEvaluationLabel(){
+		return evaluationLabel;
+	}
+
+	public void setEvaluationLabel(Label<T> label){
+		evaluationLabel = label;
 	}
 
 	@Override
