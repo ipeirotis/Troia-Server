3b82ca57af53eb4dfd02c6684a7b88efbd5d9c88
Artur Ambroziak <artur.ambroziak@10clouds.com>
2013-02-20 14:22:33 +0100

    sql changed

--- troia-server/src/main/resources/dawidprojects.sql
+++ troia-server/src/main/resources/dawidprojects.sql
@@ -1,2 +1,9 @@
-create table projects ( id varchar(100) NOT NULL PRIMARY KEY, kind varchar(25) NOT NULL, data LONGTEXT NOT NULL);
+create database dawid
+CHARACTER SET utf8
+ DEFAULT CHARACTER SET utf8
+ COLLATE utf8_general_ci
+ DEFAULT COLLATE utf8_general_ci;
+
+create table projects ( id varchar(100) NOT NULL PRIMARY KEY, kind varchar(25) NOT NULL, data LONGTEXT NOT NULL, last_use TIMESTAMP) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 create index idIndex on projects (id);
