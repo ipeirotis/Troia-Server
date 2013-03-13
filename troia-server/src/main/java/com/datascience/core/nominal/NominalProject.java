c137885cb38880a284a925978b9112a24b211354
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-03-13 14:44:26 +0100

    Moving quality related code from NominalProject to gal.Quality

--- troia-server/src/main/java/com/datascience/core/nominal/NominalProject.java
+++ troia-server/src/main/java/com/datascience/core/nominal/NominalProject.java
@@ -4,17 +4,13 @@ import com.datascience.core.base.*;
 import com.datascience.core.results.Results;
 import com.datascience.core.results.ResultsFactory;
 import com.datascience.core.results.WorkerResult;
-import com.datascience.gal.AbstractDawidSkene;
 import com.datascience.core.results.DatumResult;
 import com.datascience.core.nominal.decision.DecisionEngine;
 import com.datascience.core.nominal.decision.ILabelProbabilityDistributionCalculator;
 import com.datascience.core.nominal.decision.LabelProbabilityDistributionCalculators;
 import com.datascience.core.nominal.decision.ObjectLabelDecisionAlgorithms;
-import com.datascience.utils.ProbabilityDistributions;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * User: artur
@@ -47,112 +43,5 @@ public class NominalProject extends Project<String, NominalData, DatumResult, Wo
 		return new Results<String, DatumResult, WorkerResult>(
 				new ResultsFactory.DatumResultFactory(),
 				new ResultsFactory.WorkerResultNominalFactory(categories));
-	}
-
-	/**
-	 * @param w
-	 * @param objectCategory
-	 * @return
-	 */
-	protected Map<String, Double> getNaiveSoftLabel(Worker<String> w,
-													String objectCategory) {
-
-		HashMap<String, Double> naiveSoftLabel = new HashMap<String, Double>();
-		for (Category cat : data.getCategories()) {
-			naiveSoftLabel.put(cat.getName(), ((AbstractDawidSkene) algorithm).getErrorRateForWorker(w, objectCategory, cat.getName()));
-		}
-		return naiveSoftLabel;
-	}
-
-	/**
-	 * Gets as input a "soft label" (i.e., a distribution of probabilities over
-	 * classes) and returns the expected cost of this soft label.
-	 *
-	 * @return The expected cost of this soft label
-	 */
-	protected double getNaiveSoftLabelCost(String source,
-										   Map<String, Double> destProbabilities) {
-
-		double c = 0.0;
-		for (String destination : destProbabilities.keySet()) {
-			double p = destProbabilities.get(destination);
-			Double cost = data.getCategory(source).getCost(destination);
-			c += p * cost;
-		}
-
-		return c;
-	}
-
-	/**
-	 * Gets as input a "soft label" (i.e., a distribution of probabilities over
-	 * classes) and returns the expected cost of this soft label.
-	 *
-	 * @return The expected cost of this soft label
-	 */
-	private double getSoftLabelCost(Map<String, Double> probabilities) {
-
-		double c = 0.0;
-		for (String c1 : probabilities.keySet()) {
-			for (String c2 : probabilities.keySet()) {
-				double p1 = probabilities.get(c1);
-				double p2 = probabilities.get(c2);
-				Double cost = data.getCategory(c1).getCost(c2);
-				c += p1 * p2 * cost;
-			}
-		}
-
-		return c;
-	}
-
-	/**
-	 * Gets as input a "soft label" (i.e., a distribution of probabilities over
-	 * classes) and returns the smallest possible cost for this soft label.
-	 *
-	 * @return The expected cost of this soft label
-	 */
-	private double getMinSoftLabelCost(Map<String, Double> probabilities) {
-
-		double min_cost = Double.MAX_VALUE;
-
-		for (String c1 : probabilities.keySet()) {
-			// So, with probability p1 it belongs to class c1
-			// Double p1 = probabilities.get(c1);
-
-			// What is the cost in this case?
-			double costfor_c2 = 0.0;
-			for (String c2 : probabilities.keySet()) {
-				// With probability p2 it actually belongs to class c2
-				double p2 = probabilities.get(c2);
-				Double cost = data.getCategory(c1).getCost(c2);
-				costfor_c2 += p2 * cost;
-
-			}
-			min_cost = Math.min(min_cost, costfor_c2);
-		}
-
-		return min_cost;
-	}
-
-	/**
-	 * Returns the minimum possible cost of a "spammer" worker, who assigns
-	 * completely random labels.
-	 *
-	 * @return The expected cost of a spammer worker
-	 */
-	public double getMinSpammerCost() {
-		Map<String, Double> prior = ProbabilityDistributions.getSpammerDistribution(data);
-
-		return getMinSoftLabelCost(prior);
-	}
-
-	/**
-	 * Returns the cost of a "spammer" worker, who assigns completely random
-	 * labels.
-	 *
-	 * @return The expected cost of a spammer worker
-	 */
-	public double getSpammerCost() {
-		Map<String, Double> prior = ProbabilityDistributions.getSpammerDistribution(data);
-		return getSoftLabelCost(prior);
 	}
 }
--- troia-server/src/main/java/com/datascience/gal/Quality.java
+++ troia-server/src/main/java/com/datascience/gal/Quality.java
@@ -1,6 +1,8 @@
 package com.datascience.gal;
 
+import com.datascience.core.nominal.NominalData;
 import com.datascience.core.nominal.NominalProject;
+import com.datascience.utils.ProbabilityDistributions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,8 +13,50 @@ import java.util.Map;
  */
 public class Quality {
 
+	/**
+	 * Gets as input a "soft label" (i.e., a distribution of probabilities over
+	 * classes) and returns the smallest possible cost for this soft label.
+	 *
+	 * @return The expected cost of this soft label
+	 */
+	static private double getMinSoftLabelCost(Map<String, Double> probabilities, NominalData data) {
+
+		double min_cost = Double.MAX_VALUE;
+
+		for (String c1 : probabilities.keySet()) {
+			// So, with probability p1 it belongs to class c1
+			// Double p1 = probabilities.get(c1);
+
+			// What is the cost in this case?
+			double costfor_c2 = 0.0;
+			for (String c2 : probabilities.keySet()) {
+				// With probability p2 it actually belongs to class c2
+				double p2 = probabilities.get(c2);
+				Double cost = data.getCategory(c1).getCost(c2);
+				costfor_c2 += p2 * cost;
+
+			}
+			min_cost = Math.min(min_cost, costfor_c2);
+		}
+
+		return min_cost;
+	}
+
+	/**
+	 * Returns the minimum possible cost of a "spammer" worker, who assigns
+	 * completely random labels.
+	 *
+	 * @return The expected cost of a spammer worker
+	 */
+	static public double getMinSpammerCost(NominalData data) {
+		Map<String, Double> prior = ProbabilityDistributions.getSpammerDistribution(data);
+
+		return getMinSoftLabelCost(prior, data);
+	}
+
 	static public double fromCost(NominalProject project, double cost){
-		return 1. - cost / project.getMinSpammerCost();
+		NominalData data = project.getData();
+		return 1. - cost / getMinSpammerCost(data);
 	}
 	
 	static public Map<String, Double> fromCosts(NominalProject project, Map<String, Double> costs){
