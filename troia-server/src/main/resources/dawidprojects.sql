faa2903feb25d920c8295b7e7ae04bd86004d0c6
Panos Ipeirotis <ipeirotis@gmail.com>
2013-03-21 17:15:44 -0300

    Update dawidprojects.sql

--- troia-server/src/main/resources/dawidprojects.sql
+++ troia-server/src/main/resources/dawidprojects.sql
@@ -1,9 +1,11 @@
-create database dawid
-CHARACTER SET utf8
+CREATE DATABASE Troia 
+ CHARACTER SET utf8
  DEFAULT CHARACTER SET utf8
  COLLATE utf8_general_ci
  DEFAULT COLLATE utf8_general_ci;
 
-create table projects ( id varchar(100) NOT NULL PRIMARY KEY, kind varchar(25) NOT NULL, data LONGTEXT NOT NULL, last_use TIMESTAMP) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE Troia;
 
-create index idIndex on projects (id);
+CREATE TABLE projects ( id varchar(100) NOT NULL PRIMARY KEY, kind varchar(25) NOT NULL, data LONGTEXT NOT NULL, last_use TIMESTAMP) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE INDEX idIndex on projects (id);
