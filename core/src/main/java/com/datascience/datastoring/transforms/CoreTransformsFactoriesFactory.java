04db8ce94c1f0711b83aa594a8cb55e0f850959b
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-06-06 17:53:20 +0200

    Beginning of ThriftCoreTransformFactory and added it to tests

--- core/src/main/java/com/datascience/datastoring/transforms/CoreTransformsFactoriesFactory.java
+++ core/src/main/java/com/datascience/datastoring/transforms/CoreTransformsFactoriesFactory.java
@@ -2,6 +2,7 @@ package com.datascience.datastoring.transforms;
 
 import com.datascience.datastoring.Constants;
 import com.datascience.serialization.json.GSONSerializer;
+import com.datascience.utils.transformations.thrift.ThriftCoreTransformFactory;
 
 /**
  * @Author: konrad
@@ -14,6 +15,8 @@ public class CoreTransformsFactoriesFactory {
 			return new SerializerBasedCoreTransformsFactory(new GSONSerializer());
 		if (name == "SIMPLE")
 			return new SimpleStringCoreTransformsFactory();
+		if (name == "THRIFT")
+			return new ThriftCoreTransformFactory();
 		// TODO XXX FIXME - add protobuff, Thrift etc.
 		throw new IllegalArgumentException("Unknown serialization: " + name);
 	}
---
+++ core/src/main/java/com/datascience/utils/transformations/thrift/ThriftCoreTransformFactory.java
@@ -0,0 +1,88 @@
+package com.datascience.utils.transformations.thrift;
+
+import com.datascience.core.base.AssignedLabel;
+import com.datascience.core.base.ContValue;
+import com.datascience.core.base.LObject;
+import com.datascience.core.base.Worker;
+import com.datascience.core.nominal.PureNominalData;
+import com.datascience.core.results.*;
+import com.datascience.datastoring.transforms.ICoreTransformsFactory;
+import com.datascience.utils.ITransformation;
+import com.datascience.utils.transformations.ComposingTransform;
+import com.datascience.utils.transformations.thrift.generated.Workers;
+import com.google.gson.JsonObject;
+
+import java.io.InputStream;
+import java.util.Collection;
+
+/**
+ * @Author: konrad
+ */
+public class ThriftCoreTransformFactory implements ICoreTransformsFactory<InputStream> {
+
+	@Override
+	public ITransformation<JsonObject, InputStream> createSettingsTransform() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<String, InputStream> createKindTransform() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<Collection<AssignedLabel<String>>, InputStream> createNominalAssignsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<Collection<LObject<String>>, InputStream> createNominalObjectsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<Collection<AssignedLabel<ContValue>>, InputStream> createContAssignsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<Collection<LObject<ContValue>>, InputStream> createContObjectsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<Collection<Worker>, InputStream> createWorkersTransformation() {
+		return new ComposingTransform<Collection<Worker>, Workers, InputStream>(new ThriftCoreTransforms.WorkersTransform(),
+				new ThriftCoreTransforms.ThriftBaseTransform<Workers>(new Workers()));
+	}
+
+	@Override
+	public ITransformation<PureNominalData, InputStream> createPureNominalDataTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<DatumContResults, InputStream> createDatumContResultsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<WorkerContResults, InputStream> createWorkerContResultsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<DatumResult, InputStream> createDatumStringResultsTransformation() {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public ITransformation<WorkerResult, InputStream> createWorkerStringResultsTransformation(ResultsFactory.WorkerResultNominalFactory wrnf) {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public String getID() {
+		return "THRIFT";
+	}
+}
