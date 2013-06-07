808315976894fd01962c3c1ad8c72a32d31ac05f
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-06-07 11:19:10 +0200

    Fixing GSON serialization issues

--- core/src/test/java/com/datascience/utils/transformations/GSONTransformationTest.java
+++ core/src/test/java/com/datascience/utils/transformations/GSONTransformationTest.java
@@ -4,13 +4,11 @@ import com.datascience.datastoring.transforms.CoreTransformsFactoriesFactory;
 import com.datascience.datastoring.transforms.ICoreTransformsFactory;
 import com.datascience.datastoring.transforms.SerializerBasedCoreTransformsFactory;
 import org.junit.Assert;
-import org.junit.Ignore;
 
 /**
  * User: artur
  * Date: 5/16/13
  */
-@Ignore
 public class GSONTransformationTest extends BaseTransformationTest {
 
 	@Override
